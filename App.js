f4abc7c5655e3109d26c4214e58e45deecda75ec
Marco Túlio <marcotmotta@gmail.com>
2022-07-26 15:55:44 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [num, setNum] = useState(0)
+
+  const handleSum = () => {
+    setNum(num + 1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{num}</Text>
+      <Button title="Sum" onPress={handleSum}/>
       <StatusBar style="auto" />
     </View>
   );
